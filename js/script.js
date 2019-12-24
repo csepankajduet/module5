e6e07a0675f0a568f2c6c0fc65391df87ebdf44d
Pankaj Mallik <pankaj.mallik@sonaliintellect.com>
2019-12-24 19:15:28 +0600

    commit module5 content

--- js/script.js
+++ js/script.js
@@ -1,27 +1,73 @@
-// Event handling
-document.addEventListener("DOMContentLoaded",
-  function (event) {
-    
-    // Unobtrusive event binding
-    document.querySelector("button")
-      .addEventListener("click", function () {
-        
-        // Call server to get the name
-        $ajaxUtils
-          .sendGetRequest("data/name.txt", 
-            function (request) {
-              var name = request.responseText;
+//DOMLoader
+$(function() {
+	$(".navbar-toggle").blur(function(event){
+		var screenWidth = window.innerWidth;
+		console.log(screenWidth);
+		if (screenWidth < 768) {
+			$("#collapsable-nav").collapse('hide');
+		}
+	});
+});
 
-              document.querySelector("#content")
-                .innerHTML = "<h2>Hello " + name + "!</h2>";
-            });
+(function (global) {
+	var dc = {};
+	var homeHtml = "snippets/home-snippet.html";
+	var allCategoriesUrl = "http://davids-restaurant.herokuapp.com/categories.json";
+	var categoriesTitleHtml = "snippets/categories-title-snippet.html";
+	var categoryHtml = "snippets/category-snippet.html";
 
-        
-      });
-  }
-);
+	var insertHtml = function(selector,html){
+		var targetElem = document.querySelector(selector);
+		console.log(targetElem);
+		targetElem.innerHTML = html;
+		console.log(targetElem.innerHTML);
+	};
 
+	var showLoading = function(selector){
+		var html = "<div class='text-center'>";
+		html += "<img src='images/ajax-loader.gif'></div>";
+		insertHtml(selector,html);
+	};
 
+	var insertProperty = function(string, propName, propValue){
+		var propToReplace = "{{"+ propName +"}}";
+		string = string.replace(new RegExp(propToReplace,"g"), propValue);
+		return string;
+	}
 
+	document.addEventListener("DOMContentLoaded",function(event){
+			showLoading("#main-content");
+			$ajaxUtils.sendGetRequest(
+			homeHtml,
+			function(responseText){
+				document.querySelector("#main-content").innerHTML = responseText;
+			},
+			false);
+	});
 
+	dc.loadMenuCategories = function(){
+		showLoading("#main-content");
+		$ajaxUtils.sendGetRequest(
+			allCategoriesUrl,
+			buildAndShowCategoriesHTML
+			);
+	};
 
+	function buildAndShowCategoriesHTML(categories){
+		$ajaxUtils.sendGetRequest(
+			categoriesTitleHtml,
+			function(categoriesTitleHtml){
+				$ajaxUtils.sendGetRequest(
+					categoryHtml,
+					function(categoryHtml){
+						var categoriesViewHtml = buildCategoriesViewHtml(categories,categoriesTitleHtml,categoryHtml);
+						insertHtml ("#main-content",categoriesViewHtml);
+					},
+					false);
+			},
+	false);
+	}
+
+	global.$dc = dc;
+
+})(window);
